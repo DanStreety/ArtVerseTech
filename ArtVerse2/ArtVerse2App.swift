f93b5542f81f895e8223b35e7f7e0f19364e3006
DanStreety <148351826+DanStreety@users.noreply.github.com>
2024-04-29 06:21:22 -0400

    Initial Commit

---
+++ ArtVerse2/ArtVerse2App.swift
@@ -0,0 +1,17 @@
+//
+//  ArtVerse2App.swift
+//  ArtVerse2
+//
+//  Created by Daniel Streety on 4/29/24.
+//
+
+import SwiftUI
+
+@main
+struct ArtVerse2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
